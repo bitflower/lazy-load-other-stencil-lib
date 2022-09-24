7fe022035f394cdaea25a15bd782eb27f83c2b92
Matthias Max <matthias.max@bitflower.net>
2022-09-24 21:14:57 +0200

    .

--- src/components/SomeIonicStuff.tsx
+++ src/components/SomeIonicStuff.tsx
@@ -9,6 +9,17 @@ export const SomeIonicStuff: FunctionalComponent<any> = () => {
           <ion-label>I'm an ion list</ion-label>
         </ion-item>
       </ion-list>
+      <div>
+        <ion-segment value="default">
+          <ion-segment-button value="default">
+            <ion-label>Segment 1</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="segment">
+            <ion-label>Segment 2</ion-label>
+          </ion-segment-button>
+        </ion-segment>
+      </div>
+      <ion-datetime></ion-datetime>
     </Fragment>
   );
 };
